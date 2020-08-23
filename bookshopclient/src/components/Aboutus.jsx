a2a107adf5ac1075079b84b88da496513b077baf
kasun Chandimal <39762853+kasunChandi@users.noreply.github.com>
2020-08-23 20:06:22 +0530

    display items in the client side and add new fields to database  item collection
    add new auther for db item  collection  and display items in the client web page

---
+++ bookshopclient/src/components/Aboutus.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react' ;
+
+class Aboutus extends Component {
+    
+    state ={};
+
+    render(){
+        return (
+           <div>
+               <h1>About us page</h1>
+           </div>
+        )
+    }
+
+}
+
+
+export default Aboutus;
