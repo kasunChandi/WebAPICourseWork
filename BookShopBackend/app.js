32a479bdeeee58416cd22d5be9db710ba7ce17ab
Prabhash96isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-08-27 23:32:06 +0530

    moving admin.js to Route
    I have moved admin.js to the Route folder and made it accesible via app.js.
    
    figureing out to access database items with admin at this moment

--- BookShopBackend/app.js
+++ BookShopBackend/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const home = require('./routes/Home');
+const admin = require('./routes/admin');
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
 app.use('/api/home', home);
-
+//app.use('/admin',admin);
 
 mongoose
 .connect("mongodb://localhost/LuminexBookShopDB",{useNewUrlParser:true , useUnifiedTopology: true})
--- BookShopBackend/routes/admin.js
+++ BookShopBackend/routes/admin.js
@@ -2,13 +2,22 @@ const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
 const admin = express();  // check on it. not sure if this is correct. should it be router?
+const item = require('../models/itemSchema');
 const PORT = 5000;
 
 //still working on it. Will update soon
 
+mongoose
+.connect("mongodb://localhost/LuminexBookShopDB",{useNewUrlParser:true , useUnifiedTopology: true})
+.then(()=>console.log("*** Connected to the Database Successfully ***"))
+.catch(err=> console.log("!! connection error !!", err));
+
+//check what are the admin duties 
+
 admin.get('/admin/books/',(req,res) =>
 {
     //required code to get the books details
+    //need to get the items from the model
 });
 admin.get('/admin/books/:itemId',(req,res) =>
 {
