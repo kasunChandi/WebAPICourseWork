f2fe0056ecf1409c284cca26f0a8b306048c702a
Prabhash96isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-04 14:33:30 +0530

    Updates on the Card in Admin react

--- admin/src/components/Admin.jsx
+++ admin/src/components/Admin.jsx
@@ -10,9 +10,12 @@ class admin extends Component
           <div className="card" style={{width: "18rem"}}>
           <img src="..." className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">Book</h5>
+            <h5 className="card-title">Book Title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            <div className="btn-group" role="group" aria-label="Basic example">
+                  <button type="button" className="btn btn-secondary">Update</button>
+                  <button type="button" className="btn btn-secondary">Delete</button>
+            </div>
           </div>
         </div>
        
