ce88817c8395a513accd133dcef58a8b50db219b
Prabhash96isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-04 12:27:17 +0530

    error fixed

--- admin/src/components/Admin.jsx
+++ admin/src/components/Admin.jsx
@@ -1,21 +1,22 @@
-import React, {component} from "react";
+import React, {Component} from "react";
 
-class Admin extends component
+class admin extends Component
 {
     state = {};
 
     render()
     {
-        return (
-        <div className="card" style={{width: "18rem;"}}>
-        <img src="..." className="card-img-top" alt="..."/>
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+        return (  
+          <div className="card" style={{width: "18rem"}}>
+          <img src="..." className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h5 className="card-title">Book</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
-      </div>
-      )
+       
+        )
     }
 }
-export default Admin;   
+export default admin;   
