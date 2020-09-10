504124bff8c9e9ab1f3433205b006a71b18cb48a
Prabhash96isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-09-10 13:49:48 +0530

    updates on Admin

--- admin/src/index.js
+++ admin/src/index.js
@@ -7,15 +7,16 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import AddItem from "./components/AddItem";
 import ItemDetails from "./components/ItemDetails"
-
+import Search from './components/search';
 
 ReactDOM.render(
             <BrowserRouter>
                 <NavBar />
+                <Search/>
                 <Route exact path="/" component= {Home}/>
                 <Route exact path="/admin/items" component= {ItemList} />
                 <Route exact path="/admin/additem" component ={AddItem}/>
-                <Route exact path ="/admin/updateitem" component ={ItemDetails}/>
+              
                 
             </BrowserRouter>,   
             document.getElementById('root'));
